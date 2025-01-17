e22ab3e3b31e39c4177942a8d476354c23e2b6ad
ashwin142004 <nmashwin145@gmail.com>
2025-01-17 10:01:31 +0530

    2

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-todo1 = input("Enter a todo: ")
-todo2 = input("Enter a todo: ")
-todo3 = input("Enter a todo: ")
+promt = "Enter a todo: "
+while 2 > 1:
+    todo = input(promt)
 
-todo = [todo1, todo2, todo3]
 print(todo)
 print(type(todo))
