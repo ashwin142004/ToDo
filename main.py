8b1410b4b75a19d4ea318c03f2b71e54fcfbd3a8
ashwin142004 <nmashwin145@gmail.com>
2025-01-17 10:51:44 +0530

    2

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-promt = "Enter a todo: "
-while 2 > 1:
-    todo = input(promt)
+todos = []
+while True:
+    user_action = input("Type add, edit, show or exit:").strip()
 
-print(todo)
-print(type(todo))
+    match user_action:
+        case "add":
+            todo = input("Type your todo:")
+            todos.append(todo)
+        case "edit":   
+            todo_index = int(input("Type the number of the todo to edit:"))
+            todo_index -= 1
+            new_todo = input("Type the new todo:")
+            todos[todo_index] = new_todo
+        case "show":
+            for todo in todos:
+                print(todo)
+        case "exit":
+            print("Goodbye!")
+            break
+        case _:
+            print("Invalid action")
