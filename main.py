831f671e2e464ced12da8320eaca7d672f599b7e
ashwin142004 <nmashwin145@gmail.com>
2025-01-22 13:02:48 +0530

    4

--- main.py
+++ main.py
@@ -2,24 +2,64 @@ todos = []
 while True:
     user_action = input("Type add, edit, show, completed or exit: ").strip()
 
-    match user_action:
-        case "add":
-            todo = input("Type your todo:")
-            todos.append(todo)
-        case "edit":   
-            todo_index = int(input("Type the number of the todo to edit:"))
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+        
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+        
+        todos.append(todo + '\n')
+        
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
+    
+    elif user_action.startswith("show"):    
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+
+        for index, todo in enumerate(todos):
+            todo = todo.strip('\n')
+            row = f"{index + 1} - {todo}"
+            print(row)
+
+    elif user_action.startswith("edit"):
+        try:    
+            todo_index = int(user_action[5:])
             todo_index -= 1
-            new_todo = input("Type the new todo:")
-            todos[todo_index] = new_todo
-        case "show":
-            for index, todo in enumerate(todos):
-                print(f"{index + 1} - {todo}")
-        case "exit":
-            print("Goodbye!")
-            break
-        case "completed" | "complete":
-            todo_index = int(input("Type the number of the todo to mark as completed:"))
-            todo_index -= 1
-            todos.pop(todo_index)
-        case _:
-            print("Invalid action")
+            
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+            
+            new_todo = input("Type the new todo text:")
+            todos[todo_index] = new_todo + '\n'
+            
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+        except ValueError | IndexError:
+            print("Invalid index")
+            continue
+        
+    elif user_action.startswith("complete"):
+        try:
+            todo_index = int(user_action[9:])
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+            index = todo_index - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list"
+            print(message)  
+        except ValueError | IndexError:
+            print("Invalid index")
+            continue
+
+    elif user_action.startswith('exit'):
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid command")
+    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+new_todos = []
+            # for todo in todos:
+            #     new_todo = todo.strip('\n')
+            #     new_todos.append(new_todo)
