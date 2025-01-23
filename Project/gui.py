d7af26ccf13ac969a17a9cccf551373c37422164
ashwin142004 <nmashwin145@gmail.com>
2025-01-23 12:26:37 +0530

    final gui

--- Project/gui.py
+++ Project/gui.py
@@ -1,22 +1,33 @@
 import functions
 import FreeSimpleGUI as fsg
+import time 
 
+fsg.theme("DarkAmber")
+
+clock = fsg.Text('', key = "clock") 
 label = fsg.Text("Type in a new todo:")
 input = fsg.InputText(tooltip="Type in a new todo", key = "todo")
 add_button = fsg.Button("Add", tooltip="Add a new todo")
 list_box = fsg.Listbox(values = functions.get_todos(), key = "todos", enable_events=True ,size = (40, 10))
 edit_button = fsg.Button("Edit", tooltip="Edit a todo")
-#delete_button = fsg.Button("Delete", tooltip="Delete a todo")
+complete_button = fsg.Button("Complete", tooltip="Completed a todo")
+exit_button = fsg.Button("Exit", tooltip="Exit the program")
 
 window = fsg.Window(
     "Todo List", 
-    layout=[[label], [input, add_button], [list_box, edit_button]], 
+    layout=[
+        [clock],
+        [label], 
+        [input, add_button], 
+        [list_box, edit_button, complete_button],
+        [exit_button]], 
     font = ('Helvetica', 15)
     )
 
 while True:
-    event, values = window.read()
-    print(event, values)
+    event, values = window.read(timeout = 300)
+    window["clock"].update(value = time.strftime("%d %b %Y, %H:%M:%S"))
+ 
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -24,6 +35,8 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window["todos"].update(values = todos)
+            window["todo"].update(value = "")   
+        
         case "Edit":
             try:
                 todo_to_edit = values["todos"][0]
@@ -33,8 +46,26 @@ while True:
                 todos[index] = new_todo
                 functions.write_todos(todos)
                 window["todos"].update(values = todos)
+                window["todo"].update(value = "")
+            
             except IndexError:
                 fsg.popup("No todo selected")
+        
+        case "Complete":
+            try:
+                todo_to_remove = values["todos"][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_remove)
+                functions.write_todos(todos)
+                window["todos"].update(values = todos)
+                window["todo"].update(value = "")
+            
+            except IndexError:
+                fsg.popup("No todo selected")
+        
+        case "Exit":
+            break
+
         case "todos":
             window["todo"].update(value = values["todos"][0])
 
